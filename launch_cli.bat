a61d9542742e55a9902eed57b96fdaa7290c6963
Rylan <rdans002@fiu.edu>
2026-04-15 12:09:28 -0400

    Removed Redundant Files

--- launch_cli.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title Aegis Wireless — CLI Tool
-cd /d "C:\Users\rylan\Downloads\Test Enviorment\Aegis_Wireless\"
-python main.py
-pause
--- launch_tray.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "C:\Users\rylan\Downloads\Test Enviorment\Aegis_Wireless\"
-start /min python tray_agent.py
-exit
